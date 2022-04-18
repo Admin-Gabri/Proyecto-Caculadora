af293db3b397bddba8f3ddd708e5ebc38e3fd110
Admin-Gabri <82051935+Admin-Gabri@users.noreply.github.com>
2022-04-17 20:25:37 -0500

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Gabri\\Documents\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Gabri\\Desktop\\CLASES CERTUS\\Flutter\\Calculadora_app\\calculadora_app" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Gabri\\Documents\\flutter"
+  "PROJECT_DIR=C:\\Users\\Gabri\\Desktop\\CLASES CERTUS\\Flutter\\Calculadora_app\\calculadora_app"
+  "FLUTTER_ROOT=C:\\Users\\Gabri\\Documents\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Gabri\\Desktop\\CLASES CERTUS\\Flutter\\Calculadora_app\\calculadora_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Gabri\\Desktop\\CLASES CERTUS\\Flutter\\Calculadora_app\\calculadora_app"
+  "FLUTTER_TARGET=C:\\Users\\Gabri\\Desktop\\CLASES CERTUS\\Flutter\\Calculadora_app\\calculadora_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Gabri\\Desktop\\CLASES CERTUS\\Flutter\\Calculadora_app\\calculadora_app\\.dart_tool\\package_config.json"
+)
